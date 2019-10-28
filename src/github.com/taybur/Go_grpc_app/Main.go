633b75453fe847ae898d26ae22630927a062aec4
root <root@localhost.localdomain>
2019-10-29 04:28:41 +0600

    added this first

---
+++ src/github.com/taybur/Go_grpc_app/Main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+
+	"github.com/docker/docker/pkg/reexec"
+)
+
+func init() {
+	reexec.Register("nsInitialisation", nsInitialisation)
+	if reexec.Init() {
+		os.Exit(0)
+	}
+}
+
+func nsInitialisation() {
+	newrootPath := os.Args[1]
+
+	if err := mountProc(newrootPath); err != nil {
+		fmt.Printf("Error mounting /proc - %s\n", err)
+		os.Exit(1)
+	}
+
+	if err := pivotRoot(newrootPath); err != nil {
+		fmt.Printf("Error running pivot_root - %s\n", err)
+		os.Exit(1)
+	}
+
+	if err := syscall.Sethostname([]byte("sharif_info")); err != nil {
+		fmt.Printf("Error setting hostname - %s\n", err)
+		os.Exit(1)
+	}
+
+	if err := waitForNetwork(); err != nil {
+		fmt.Printf("Error waiting for network - %s\n", err)
+		os.Exit(1)
+	}
+
+	nsRun()
+}
+
+func nsRun() {
+	cmd := exec.Command("/bin/sh")
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.Env = []string{"PS1=-[ns-process]- # "}
+
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("Error running the /bin/sh command - %s\n", err)
+		os.Exit(1)
+	}
+}
+
+func main() {
+	var rootfsPath, netsetgoPath string
+	flag.StringVar(&rootfsPath, "rootfs", "/tmp/ns-process/rootfs", "Path to the root filesystem to use")
+	flag.StringVar(&netsetgoPath, "netsetgo", "/usr/local/bin/netsetgo", "Path to the netsetgo binary")
+	flag.Parse()
+
+	exitIfRootfsNotFound(rootfsPath)
+	exitIfNetsetgoNotFound(netsetgoPath)
+
+	cmd := reexec.Command("nsInitialisation", rootfsPath)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWNS |
+			syscall.CLONE_NEWUTS |
+			syscall.CLONE_NEWIPC |
+			syscall.CLONE_NEWPID |
+			syscall.CLONE_NEWNET |
+			syscall.CLONE_NEWUSER,
+		UidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID:      os.Getuid(),
+				Size:        1,
+			},
+		},
+		GidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID:      os.Getgid(),
+				Size:        1,
+			},
+		},
+	}
+
+	if err := cmd.Start(); err != nil {
+		fmt.Printf("Error starting the reexec.Command - %s\n", err)
+		os.Exit(1)
+	}
+
+	// run netsetgo using default args
+	// note that netsetgo must be owned by root with the setuid bit set
+	pid := fmt.Sprintf("%d", cmd.Process.Pid)
+	netsetgoCmd := exec.Command(netsetgoPath, "-pid", pid)
+	if err := netsetgoCmd.Run(); err != nil {
+		fmt.Printf("Error running netsetgo - %s\n", err)
+		os.Exit(1)
+	}
+
+	if err := cmd.Wait(); err != nil {
+		fmt.Printf("Error waiting for the reexec.Command - %s\n", err)
+		os.Exit(1)
+	}
+}
